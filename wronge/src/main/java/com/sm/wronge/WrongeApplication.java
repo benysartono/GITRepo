5708c5a9ae11ce1a20cec2e49ab68faee01fa6e9
benysartono <sartonobeny@yahoo.com>
2023-12-19 17:50:53 +0700

    20231219-1

---
+++ wronge/src/main/java/com/sm/wronge/WrongeApplication.java
@@ -0,0 +1,13 @@
+package com.sm.wronge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrongeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrongeApplication.class, args);
+	}
+
+}
